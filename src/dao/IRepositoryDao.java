09c27676a70f50edc18b5bd8318cdedb2e8d5b07
DNyaika <davie2086@gmail.com>
2016-05-17 22:30:12 +0200

    adding dao implementation

--- src/dao/IRepositoryDao.java
+++ src/dao/IRepositoryDao.java
@@ -1,12 +1,13 @@
 package dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IRepositoryDao<T> {
 	
-	public void save(T t);
-	public void update(T t);
-	public void delete(T t);
-	public List<T> getAll();
-	public T get(T t);
+	public int save(T t)throws SQLException;
+	public void update(T t)throws SQLException;
+	public void delete(T t)throws SQLException;
+	public List<T> getAll(String entityName)throws SQLException;
+	public T get(T t)throws SQLException;
 }
