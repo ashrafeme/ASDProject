88f3b682a0c64becce2c67aad0323eaeb6720d6b
ashrafeme <ashraf_eme@hotmail.com>
2016-05-17 12:01:24 -0500

    person class

---
+++ src/model/Person.java
@@ -0,0 +1,32 @@
+package model;
+
+public abstract class Person {
+
+	private String fName;
+	private String lName;
+	private String dob;
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+}
