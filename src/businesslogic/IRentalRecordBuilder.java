d90e15b3ccec0c83fe7f82403fb399db59de8316
Tito Suarez <tito.suarez.b@gmail.com>
2016-05-18 15:06:09 -0500

    JavaDocs

--- src/businesslogic/IRentalRecordBuilder.java
+++ src/businesslogic/IRentalRecordBuilder.java
@@ -7,10 +7,22 @@ import model.Item;
 
 public interface IRentalRecordBuilder {
 	
+	/**
+	 * Sets the employee who is registering the rental record
+	 * @param Employee employee
+	 */
 	public void setEmployee(Employee employee);
 
+	/**
+	 * Sets the customer who is borrowing the Items
+	 * @param Customer customer
+	 */
 	public void setCustomer(Customer customer);
 
+	/**
+	 * 
+	 * @param item
+	 */
 	public void addItem(Item item);
 
 	public AbstractRentalRecord getRentalRecord();
