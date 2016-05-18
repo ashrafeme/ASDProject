5b15dd74fdd83c8ae38675628c69753dd7d24379
DNyaika <davie2086@gmail.com>
2016-05-18 19:08:37 +0200

    addding changes to RentalRecord

--- src/model/AbstractRentalRecord.java
+++ src/model/AbstractRentalRecord.java
@@ -1,5 +1,20 @@
 package model;
 
-public class AbstractRentalRecord {
+import java.util.Date;
 
+import businesslogic.*;
+
+public abstract class AbstractRentalRecord {
+
+	public final void beginRentalProcess(Payment payment) {
+		processPayment(payment);
+	}
+
+	public final void closeRecord(Date date) {
+		calculateFine(date);
+	}
+
+	public abstract void processPayment(Payment payment);
+
+	public abstract void calculateFine(Date date);
 }
--- src/model/RentalRecord.java
+++ src/model/RentalRecord.java
@@ -1,8 +1,23 @@
 package model;
 
-public class RentalRecord extends AbstractRentalRecord{
-	
+import java.util.Date;
+
+import businesslogic.Payment;
+
+public class RentalRecord extends AbstractRentalRecord {
+
 	private Employee employee;
-	
+
+	@Override
+	public void processPayment(Payment payment) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void calculateFine(Date date) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
